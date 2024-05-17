bd0f73d2138e166dbf469c15fb351135672bf8f1
mahadev <mahadev@test.com>
2024-05-16 22:30:16 -0500

    Commit test added comments

--- src/main/java/com/maha/examples/SpringBootWsapiApplication.java
+++ src/main/java/com/maha/examples/SpringBootWsapiApplication.java
@@ -11,6 +11,7 @@ public class SpringBootWsapiApplication {
 		SpringApplication.run(SpringBootWsapiApplication.class, args);
 	}
 
+	// Git commnads 
 	
 //	…or create a new repository on the command line
 //
@@ -18,14 +19,14 @@ public class SpringBootWsapiApplication {
 //	git init
 //	git add README.md
 //	git commit -m "first commit"
-//	git branch -M main
+//	git branch -M main >> //	git branch -M master
 //	git remote add origin https://github.com/MahaCloud/MahaWS.git
 //	git push -u origin main
 //
 //	…or push an existing repository from the command line
 //	git remote add origin https://github.com/MahaCloud/MahaWS.git
-//	git branch -M main
-//	git push -u origin main
+//	git branch -M main >> git branch -M master
+//	git push -u origin main  >>  git push -u origin master
 	
 }
 
